356e63ce03820f43c899fec6f6b9ac16210baf64
RoosterKingII <moidavidheru2@gmail.com>
2025-01-08 13:24:10 -0400

    Add files via upload

--- Client/ModuleManager/ModuleManager.cpp
+++ Client/ModuleManager/ModuleManager.cpp
@@ -7,16 +7,31 @@ ModuleManager::ModuleManager() {
 		//Combat
 		moduleList.push_back(new Killaura());
 		moduleList.push_back(new AutoCrystal());
+		moduleList.push_back(new Killaura2());
 		moduleList.push_back(new Reach());
+		moduleList.push_back(new Switcher());
 		moduleList.push_back(new Hitbox());
-		moduleList.push_back(new AnchorAura());
+		moduleList.push_back(new AutoTrap());
+		moduleList.push_back(new Godmode());
 		moduleList.push_back(new AnchorNeo());
+		moduleList.push_back(new AnchorAura());
+		//push_back(new octopus());
+		moduleList.push_back(new AutoTotem());
+		moduleList.push_back(new TPAura());
+		moduleList.push_back(new Shoreline());
 		//Misc
 		moduleList.push_back(new NoPacket());
 		moduleList.push_back(new Timer());
+		moduleList.push_back(new Origem());
 		moduleList.push_back(new AntiCrystal());
 		moduleList.push_back(new Clip());
+		moduleList.push_back(new FallCounter());
+		moduleList.push_back(new LifeboatReach());
+		moduleList.push_back(new Blink());
+		moduleList.push_back(new DeathCoordinates());
 		moduleList.push_back(new Disabler());
+		//moduleList.push_back(new ProxyFake());
+		moduleList.push_back(new DamageText());
 		moduleList.push_back(new ChatSuffix());
 		//Render
 		moduleList.push_back(new CameraNoClip());
@@ -24,43 +39,51 @@ ModuleManager::ModuleManager() {
 		moduleList.push_back(new Fullbright());
 		moduleList.push_back(new HurtColor());
 		moduleList.push_back(new NameTags());
+		moduleList.push_back(new BlockOutline());
+		moduleList.push_back(new BlockESP());
 		moduleList.push_back(new StorageESP());
 		moduleList.push_back(new NoHurtcam());
 		moduleList.push_back(new NoRender());
 		moduleList.push_back(new Tracer());
 		moduleList.push_back(new ESP());
 		moduleList.push_back(new CustomSky());
+		moduleList.push_back(new Playerlist());
 		//Movement
-		//moduleList.push_back(new Airjump()); later
 		moduleList.push_back(new AutoSprint());
 		moduleList.push_back(new Fly());
+		moduleList.push_back(new Glide());
+		moduleList.push_back(new Jetpack());
 		moduleList.push_back(new Velocity());
+		moduleList.push_back(new NoClip());
 		moduleList.push_back(new Phase());
+		moduleList.push_back(new Speed());
 		moduleList.push_back(new ElytraFly());
 		moduleList.push_back(new NoSlowDown());
 		//Player
 		moduleList.push_back(new Antibot());
+		moduleList.push_back(new TeleportModule());
 		moduleList.push_back(new BlockReach());
+		moduleList.push_back(new Scaffold());
 		moduleList.push_back(new FastEat());
+		moduleList.push_back(new PlayerJoin());
 		moduleList.push_back(new PacketMine());
+		moduleList.push_back(new BasePlace());
 		moduleList.push_back(new Swing());
-		moduleList.push_back(new Surround());
-		moduleList.push_back(new AutoOffhand());
+		//moduleList.push_back(new Surround());
+		//moduleList.push_back(new AutoOffhand());
+		moduleList.push_back(new MidClick());
 		//Client
 		moduleList.push_back(new ArrayList());
-		//moduleList.push_back(new Clients());
 		moduleList.push_back(new Colors());
 		moduleList.push_back(new CustomFont());
 		moduleList.push_back(new ClickGui());
 		moduleList.push_back(new HUD());
 		moduleList.push_back(new Notifications());
 		moduleList.push_back(new Config121());
-		//Build
-		//moduleList.push_back(new Build());
 	}
 	std::sort(moduleList.begin(), moduleList.end(), [](Module* lhs, Module* rhs) {
 		return lhs->getModuleName() < rhs->getModuleName();
-	});
+		});
 	getModule("ClickGui")->setEnabled(false);
 	getModule("Notifications")->setEnabled(true);
 	getModule("HUD")->setEnabled(true);
--- Client/ModuleManager/ModuleManager.h
+++ Client/ModuleManager/ModuleManager.h
@@ -9,24 +9,42 @@
 #include "Modules/Module.h"
 
 //Combat
+#include "Modules/Combat/AutoTrap.h"
+#include "Modules/Combat/octopus.h"
 #include "Modules/Combat/Killaura.h"
+#include "Modules/Combat/Killaura2.h"
 #include "Modules/Combat/Hitbox.h"
 #include "Modules/Combat/AutoCrystal.h"
+#include "Modules/Combat/Switcher.h"
 #include "Modules/Combat/Reach.h"
+#include "Modules/Combat/TPAura.h"
+#include "Modules/Combat/Godmode.h"
 #include "Modules/Combat/AnchorAura.h"
 #include "Modules/Combat/AnchorNeo.h"
+#include "Modules/Combat/AutoTotem.h"
+#include "Modules/Player/shorelineselftrap.h"
+
 //Misc
 #include "Modules/Misc/NoPacket.h"
 #include "Modules/Misc/Timer.h"
+#include "Modules/Misc/Origem.h"
 #include "Modules/Misc/Anticrystal.h"
 #include "Modules/Misc/ChatSuffix.h"
 #include "Modules/Misc/Disabler.h"
+#include "Modules/Misc/LbReach.h"
+#include "Modules/Misc/DeathCoords.h"
+#include "Modules/Misc/ProxyFake.h"
+#include "Modules/Misc/DamageText.h"
+#include "Modules/Misc/FallCounter.h"
+#include "Modules/Misc/Blink.h"
 #include "Modules/Misc/Clip.h"
 //Render
 #include "Modules/Render/CameraNoClip.h"
 #include "Modules/Render/CustomFov.h"
 #include "Modules/Render/Fullbright.h"
 #include "Modules/Render/HurtColor.h"
+#include "Modules/Render/BlockOutline.h"
+#include "Modules/Render/BlockESP.h"
 #include "Modules/Render/NameTags.h"
 #include "Modules/Render/NoHurtcam.h"
 #include "Modules/Render/NoRender.h"
@@ -34,22 +52,31 @@
 #include "Modules/Render/ESP.h"
 #include "Modules/Render/StorageESP.h"
 #include "Modules/Render/CustomSky.h"
+#include "Modules/Render/Playerlist.h"
 //Movement
-#include "Modules/Movement/Airjump.h"
 #include "Modules/Movement/AutoSprint.h"
 #include "Modules/Movement/Velocity.h"
 #include "Modules/Movement/Fly.h"
+#include "Modules/Movement/Jetpack.h"
+#include "Modules/Movement/NoClip.h"
 #include "Modules/Movement/ElytraFly.h"
 #include "Modules/Movement/NoSlowDown.h"
+#include "Modules/Movement/Speed.h"
 #include "Modules/Movement/Phase.h"
+#include "Modules/Movement/Glide.h"
 //Player
 #include "Modules/Player/BlockReach.h"
 #include "Modules/Player/FastEat.h"
 #include "Modules/Player/PacketMine.h"
+#include "Modules/Player/Scaffold.h"
 #include "Modules/Player/Swing.h"
+#include "Modules/Player/PlayerJoin.h"
 #include "Modules/Player/Surround.h"
+#include "Modules/Player/TeleportModule.h"
+#include "Modules/Player/BasePlace.h"
 #include "Modules/Player/Antibot.h"
 #include "Modules/Player/AutoOffhand.h"
+#include "Modules/Player/MidClick.h"
 //Client
 #include "Modules/Client/ArrayList.h"
 #include "Modules/Client/ClickGui.h"
@@ -63,16 +90,16 @@
 
 class ModuleManager {
 private:
-	std::vector<Module*> moduleList;
+    std::vector<Module*> moduleList;
     bool initialized = false;
 public:
-	ModuleManager();
+    ModuleManager();
     ~ModuleManager();
 public:
     inline std::vector<Module*>* getModuleList() { return &this->moduleList; }
     inline const bool isInitialized() { return this->initialized; }
 
-	Module* getModule(const std::string& moduleName) {
+    Module* getModule(const std::string& moduleName) {
         std::string nameLower(moduleName);
         std::transform(nameLower.begin(), nameLower.end(), nameLower.begin(), ::tolower);
 
@@ -82,7 +109,7 @@ public:
             if (strcmp(nameLower.c_str(), modNameLower.c_str()) == 0) return mod;
         }
         return nullptr;
-	}
+    }
     std::optional<Module*> getModuleByName(const std::string& name) {
         std::string nameLower = name;
         std::transform(nameLower.begin(), nameLower.end(), nameLower.begin(), ::tolower);
@@ -98,7 +125,7 @@ public:
         return std::nullopt;
     }
 
-    
+
 
 
 public:
