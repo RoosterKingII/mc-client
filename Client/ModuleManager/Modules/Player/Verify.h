7428aac894a86cae52b8a05990285880a9f1990c
RoosterKingII <moidavidheru2@gmail.com>
2025-01-10 22:27:29 -0400

    Create Verify.h

---
+++ Client/ModuleManager/Modules/Player/Verify.h
@@ -0,0 +1,22 @@
+#pragma once
+#include "../Module.h"
+#include <unordered_set>
+#include <string>
+
+class Verify : public Module {
+private:
+    std::unordered_set<std::string> allowedPlayers;
+    std::string oneDriveUrl;  
+    std::string fileName;     
+    bool downloadPlayerList();
+    bool loadAllowedPlayers();
+    bool isPlayerAllowed(const std::string& playerName);
+    void LogError(const std::string& message);
+    int retryCount = 0;
+    const int MAX_RETRIES = 3;
+
+public:
+    Verify();
+    virtual void onEnable() override;
+    virtual void onDisable() override;
+};
