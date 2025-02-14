0ea956aea79f4ede1c59708ed8216e71a64db508
RoosterKingII <moidavidheru2@gmail.com>
2025-02-13 22:24:20 -0400

    Update Fly.h

--- Client/ModuleManager/Modules/Movement/Fly.h
+++ Client/ModuleManager/Modules/Movement/Fly.h
@@ -1,51 +1,34 @@
 #pragma once
 #include "../Module.h"
 
-class Fly : public Module {
+class Fly2 : public Module {
 private:
-	int IsWpressed = 0;
-	int Modefly = 0;
-	int lastMoveTime = 0;
-	int DelayUpDown = 0;
-	float speed = 0.325f;
-	float Glide = -0.2f;
-	float glideMod = -0.00f;
-	float glideModEffective = 0;
-	Vec3<float> oldPos;
-	bool smborder;
-	bool NoPacket = false;
+    int IsWpressed = 0;
+    int Modefly = 0;
+    int lastMoveTime = 0;
+    int DelayUpDown = 0;
+    float speed = 0.325f;
+    float Glide = -0.2f;
+    float glideMod = -0.00f;
+    float glideModEffective = 0;
+    float waterSpeedMultiplier = 0.7f;
+    Vec3<float> oldPos;
+    bool smborder=false;
+    bool NoPacket = false;
+
+    // Parámetros para normalización por distancia
+    int distanceThreshold;
+    float speedMultiplierMin = 0.1f;
+    float normalizationStrength = 1.0f;
 
 public:
-	float upanddown = 0.6f;
-	Fly();
-	float HorizontalSpeed = 1.f;
-	~Fly();
+    float upanddown = 0.6f;
+    float HorizontalSpeed = 1.f;
 
-	virtual void onNormalTick(Actor* actor) override;
-	virtual void onEnable() override;
-	virtual void onDisable() override;
+    Fly2();
+    ~Fly2();
+
+    virtual void onNormalTick(Actor* actor) override;
+    virtual void onEnable() override;
+    virtual void onDisable() override;
 };
-
-/*#pragma once
-#include "../Module.h"
-
-class Fly : public Module {
-private:
-	float speed = 1.9f;
-	float upanddown = 1.8f;
-	bool sneak = false;
-	float glideMod = -0.02f;
-	float glideModEffective = 0;
-	Vec3<float> oldPos;
-	bool smborder;
-	bool NoPacket = false;
-
-public:
-	Fly();
-	~Fly();
-
-	virtual void onNormalTick(Actor* actor) override;
-	virtual void onEnable() override;
-	virtual void onDisable() override;
-};
-*/
