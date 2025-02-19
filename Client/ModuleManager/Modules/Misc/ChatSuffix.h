73b47cc9eb545ff69ea5430195facf5a450729a6
RoosterKingII <moidavidheru2@gmail.com>
2025-02-19 13:51:22 -0400

    Update ChatSuffix.h

--- Client/ModuleManager/Modules/Misc/ChatSuffix.h
+++ Client/ModuleManager/Modules/Misc/ChatSuffix.h
@@ -1,9 +1,16 @@
 #pragma once
 #include "../Module.h"
+#include <random>
+#include <vector>
 
 class ChatSuffix : public Module {
-public:
-	ChatSuffix();
+private:
+    bool green = true;
+    std::vector<std::string> suffixes;
+    std::mt19937 rng;
 
-	virtual void onSendPacket(Packet* packet, bool& shouldCancel) override;
+public:
+    ChatSuffix();
+    virtual void onSendPacket(Packet* packet, bool& shouldCancel) override;
+    std::string getRandomSuffix();
 };
