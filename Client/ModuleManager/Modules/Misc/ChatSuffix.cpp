9e88680ae3f5d103de33917fbdd1deb8665bb917
RoosterKingII <moidavidheru2@gmail.com>
2025-02-19 15:22:27 -0400

    Update ChatSuffix.cpp

--- Client/ModuleManager/Modules/Misc/ChatSuffix.cpp
+++ Client/ModuleManager/Modules/Misc/ChatSuffix.cpp
@@ -1,54 +1,37 @@
 #include "ChatSuffix.h"
 
 ChatSuffix::ChatSuffix() : Module("ChatSuffix", "Add a random suffix to your chat messages", Category::MISC) {
-    // Inicializar el generador de números aleatorios
     std::random_device rd;
     rng = std::mt19937(rd());
-    
-    // Inicializar vector de sufijos
+
     suffixes = {
-        " | Desmariquead@ si lo lees",
-		" | Desmariquead@ si lo ves",
-        " | EZZZ",
-		" | gg",
-		" | LMAO",
-		" | LUL",
-		" | 1v1?",
-        " | horionclien",
-        " | NukeMobile",
-        " | biba Dios",
-        " | Put@ Client",
-
-		" | Put@ Horion"
-        " | el que se mueva es gei",
-
-		" | el que lea es gei",
-		" | el que hable es gei",
-		" | el que respire es gei",
-		" | el que exista es gei",
-		" | el que juegue es gei",
-        " | si lees esto eres marica",
-
-		" | si lees esto eres gei",
-        
-        " | Me cojo al mudo",
-        
-        " | Me cojo al que hable",
-
-		
-        " | Me cojo al que lea",
-
-		" | Me cojo al mundo",
-
-		" | Me cojo a todos",
-		
-        " | MARIC@",
-
-		" | GEI",
-		" | ʜitler client",
-		" | ",
-
-
+          " | Desmariquead@ si lo lees",
+          " | Desmariquead@ si lo ves",
+          " | EZZZ",
+          " | gg",
+          " | LMAO",
+          " | LUL",
+          " | 1v1?",
+          " | horionclien",
+          " | NukeMobile",
+          " | biba Dios",
+          " | Put@ Client",
+          " | Put@ Horion"
+          " | el que se mueva es gei",
+          " | el que lea es gei",
+          " | el que hable es gei",
+          " | el que respire es gei",
+          " | el que exista es gei",
+          " | el que juegue es gei",
+          " | si lees esto eres marica",
+          " | si lees esto eres gei",
+          " | Me cojo al mudo",
+          " | Me cojo al que hable",
+          " | Me cojo al que lea",
+          " | Me cojo al mundo",
+          " | Me cojo a todos",
+          " | MARIC@",
+          " | GEI",
     };
 }
 
@@ -60,17 +43,14 @@ std::string ChatSuffix::getRandomSuffix() {
 void ChatSuffix::onSendPacket(Packet* packet, bool& shouldCancel) {
     if (packet->getId() == PacketID::Text) {
         TextPacket* textPacket = reinterpret_cast<TextPacket*>(packet);
-        std::string message = textPacket->message;
-        
-        // Agregar prefijo ">" si green está activado
+        std::string message = textPacket->message.getText(); 
+
         if (green) {
             message = ">" + message;
         }
-        
-        // Agregar sufijo aleatorio
+
         message += getRandomSuffix();
-        
-        // Actualizar el mensaje del paquete
-        textPacket->message = message;
+
+        textPacket->message = TextHolder(message); 
     }
 }
