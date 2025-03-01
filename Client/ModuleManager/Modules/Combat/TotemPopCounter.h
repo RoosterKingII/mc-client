5a35585691b28a781f440480157cd2084bdc924a
RoosterKingII <moidavidheru2@gmail.com>
2025-03-01 11:44:54 -0400

    Update TotemPopCounter.h

--- Client/ModuleManager/Modules/Combat/TotemPopCounter.h
+++ Client/ModuleManager/Modules/Combat/TotemPopCounter.h
@@ -1,32 +1,12 @@
 #pragma once
 #include "../Module.h"
 #include <map>
-#include <sstream>
-#include <string>
 
 class TotemPopCounter : public Module {
 private:
     std::map<std::string, int> popList;
     std::map<std::string, bool> totemEquipped;
-    bool sendClientMessage = false;
-    bool sendGlobalChat = false;
-
-    void sendChat(std::string text, ...) {
-        va_list arg;
-        va_start(arg, text);
-
-        char message[300];
-        vsprintf_s(message, 300, text.c_str(), arg);
-        va_end(arg);
-
-        TextPacket textPacket;
-        textPacket.message.setText(std::string(message));
-        textPacket.userName.setText(mc.getLocalPlayer()->getNameTag());
-        textPacket.xboxUserId = std::to_string(mc.getLocalPlayer()->getUserId());
-
-        mc.getClientInstance()->loopbackPacketSender->sendToServer(&textPacket);
-
-    }
+    bool sendChat = true;
 
 public:
     TotemPopCounter();
