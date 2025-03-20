dcf2987ee74b18588afc2a2c7171162cba184c82
RoosterKingII <moidavidheru2@gmail.com>
2025-03-20 12:38:57 -0400

    Update ChatSuffix.h

--- Client/ModuleManager/Modules/Misc/ChatSuffix.h
+++ Client/ModuleManager/Modules/Misc/ChatSuffix.h
@@ -1,3 +1,4 @@
+// ChatSuffix.h
 #pragma once
 #include "../Module.h"
 #include <random>
@@ -6,6 +7,10 @@
 class ChatSuffix : public Module {
 private:
     bool green = true;
+    bool toxicSuffix = true;    // Para el suffix tóxico original
+    bool clientSuffix = false;  // Para mostrar "SX1 Client"
+    bool randomString = false;  // Para el string aleatorio
+    int randomStringLength = 8; // Longitud del string aleatorio
     std::vector<std::string> suffixes;
     std::mt19937 rng;
 
@@ -13,4 +18,5 @@ public:
     ChatSuffix();
     virtual void onSendPacket(Packet* packet, bool& shouldCancel) override;
     std::string getRandomSuffix();
+    std::string generateRandomString(int length);
 };
